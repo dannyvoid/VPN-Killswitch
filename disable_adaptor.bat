e9fe7925e447195adb69c01c367c3926bb67f3de
Danny Void <danielilluminatic@gmail.com>
2017-10-17 00:13:30 -0500

    Create disable_adaptor.bat

---
+++ disable_adaptor.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+
+NET SESSION >NUL
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE >NUL
+goto :start
+
+:ELEVATE
+CD /d %~dp0
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();" >NUL
+EXIT
+
+:start
+netsh interface set interface Ethernet DISABLED >NUL
