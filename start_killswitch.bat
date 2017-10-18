caf0c0908cc09afbe548bd5236c211962d89cd41
Danny Void <danielilluminatic@gmail.com>
2017-10-18 02:32:12 -0500

    Update start_killswitch.bat

--- start_killswitch.bat
+++ start_killswitch.bat
@@ -1,2 +1,14 @@
 @echo off
+
+NET SESSION >NUL
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE >NUL
+goto :start
+
+:ELEVATE
+CD /d %~dp0
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();" >NUL
+EXIT
+
+:start
+cd "C:\Path\To\VPN-Killswitch"
 python app.py
