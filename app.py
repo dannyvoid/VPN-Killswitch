bd3531366cf7ea3bff5c926b4e7a60b363b47bf9
Danny Void <danielilluminatic@gmail.com>
2017-10-19 20:09:01 -0500

    cleaned up the logic

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import time
 import ctypes
+import socket
 import logging
 import requests
 import subprocess
@@ -12,15 +13,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 network_adapter = 'Ethernet'
 
-xfinity_prefixes = ["24.0", "24.16", "24.30", "24.34", "24.60", "24.91",
-"24.98", "24.118", "24.125", "24.126", "24.128", "24.129", "24.130", "24.147",
-"24.218", "24.245", "50.128", "65.96", "66.30", "66.41", "66.56", "66.176",
-"66.229", "67.160", "67.176", "67.180", "67.184", "68.32", "68.80", "68.84",
-"69.136", "69.138", "69.139", "69.140", "69.180", "69.242", "69.244", "69.248",
-"65.34.128", "69.253", "69.254 ", "71.56", "71.192", "71.224", "73.", "75.64",
-"75.72", "75.74", "75.75", "75.75", "76.16", "76.97", "76.98", "76.100",
-"76.104", "76.112", "98.192", "98.200", "98.204", "98.206", "98.208", "98.224",
-"98.240", "98.242", "98.244", "98.248", "107.2", "107.4", "174.48"]
+xfinity_prefixes = ['24.0', '24.16', '24.30', '24.34', '24.60', '24.91',
+'24.98', '24.118', '24.125', '24.126', '24.128', '24.129', '24.130', '24.147',
+'24.218', '24.245', '50.128', '65.96', '66.30', '66.41', '66.56', '66.176',
+'66.229', '67.160', '67.176', '67.180', '67.184', '68.32', '68.80', '68.84',
+'69.136', '69.138', '69.139', '69.140', '69.180', '69.242', '69.244', '69.248',
+'65.34.128', '69.253', '69.254 ', '71.56', '71.192', '71.224', '73.', '75.64',
+'75.72', '75.74', '75.75', '75.75', '76.16', '76.97', '76.98', '76.100',
+'76.104', '76.112', '98.192', '98.200', '98.204', '98.206', '98.208', '98.224',
+'98.240', '98.242', '98.244', '98.248', '107.2', '107.4', '174.48']
 
 def welcome():
     stamp = str(datetime.now())
@@ -28,29 +29,37 @@ def welcome():
     print('###  VPN-Killswitch 1.0  ###')
     print('### Created by DannyVoid ###')
     print('############################')
-    print('\nStarted at %s' % (stamp))
+    print('\nStarted at {}'.format(stamp))
     print('-------------------------------------\n')
 
-def check_if_vpn():
+def is_online():
     try:
-        ip = get('https://api.ipify.org').text
+        socket.create_connection(('www.google.com', 80))
+        return True
+    except Exception:
+        pass
+    return False
 
-        if ip.startswith(tuple(xfinity_prefixes)):
-            stamp = str(datetime.now())
-            print('VPN Error! - %s' % (stamp))
-            subprocess.call('netsh interface set interface %s DISABLED' % (str(network_adapter)))
-
-        else:
-            stamp = str(datetime.now())
-            print('VPN Enabled! - %s' % (stamp))
-
-    except requests.ConnectionError:
+def vpn_check():
+    if is_online():
+        try:
+            ip = get('https://api.ipify.org').text
+            if ip.startswith(tuple(xfinity_prefixes)):
+                stamp = str(datetime.now())
+                print('VPN Not Detected! {}'.format(stamp))
+                subprocess.call('netsh interface set interface {} DISABLED'.format(network_adapter), stdout=open(os.devnull, 'wb'))
+            else:
+                stamp = str(datetime.now())
+                print('VPN Working!      {}'.format(stamp))
+        except Exception:
+            pass
+    else:
         stamp = str(datetime.now())
-        print('Network Offline. - %s' % (stamp))
+        print('Network Offline!  {}'.format(stamp))
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(check_if_vpn, 'interval', seconds=10)
+    scheduler.add_job(vpn_check, 'interval', seconds=5)
     scheduler.start()
     welcome()
 
